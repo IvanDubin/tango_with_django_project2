100ea88dcb29dc662a3cde8fbf34342d88424b7d
Ivan Dubin <2252507d@student.gla.ac.uk>
2018-02-05 13:20:11 +0000

    ch5\6 problems

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
+from rango.models import Category 
+from rango.models import Page
+def index(request): 
+	# Query the database for a list of ALL categories currently stored. 
+	# Order the categories by no. likes in descending order. 
+	# Retrieve the top 5 only - or all if less than 5. 
+	# Place the list in our context_dict dictionary 
+	# that will be passed to the template engine.
+	category_list = Category.objects.order_by('-likes')[:5]
+	context_dict = {'categories': category_list}
+	for category in category_list:
+		category.url = category.name.replace(' ', '_')
+	# Render the response and send it back! 
+	return render(request, 'rango/index.html', context_dict)
 
-def index(request):
-	context_dict = {'boldmessage' : "Crunchy, creamy, cookie, candy, cupcake!"}
-	return render(request, 'rango/index.html', context=context_dict)
