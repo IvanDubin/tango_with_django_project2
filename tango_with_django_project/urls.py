a3a7ed50afb8ee9606a85f489b9f61faa2345f31
Ivan Dubin <2252507d@student.gla.ac.uk>
2018-02-08 21:52:07 +0000

    7 working

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -20,8 +20,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 from rango import views
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-	url(r'^rango/', include('rango.urls')),
-	
-	url(r'^admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    url(r'$', views.index, name='index'),
+    url(r'about/$', views.about, name='about'),
+    url(r'^add_category/$', views.add_category, name='add_category'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
+        views.show_category, name='show_category'),
+   ]
